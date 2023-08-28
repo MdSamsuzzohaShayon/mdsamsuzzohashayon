50456a7b425b5f4dbfe0f52308ae6c34e12bc076
Md Shayon <mdshayon0@gmail.com>
2023-08-29 05:04:10 +0600

    Previous one was working, lets try this

--- pyserver/main/__init__.py
+++ pyserver/main/__init__.py
@@ -98,7 +98,7 @@ class SendEmailModal(BaseModel):
 @app.get("/api/test")
 def test_api():
     try:
-        return {"message": "Send email with Azure Functions (Update)!"}
+        return {"message": "Send email with Azure Functions (Update-1)!"}
     except Exception as e:
         raise HTTPException(status_code=404, detail="Item not found")
 
